53082ea34143e7bb5898e9ab68a90d11e27c1120
DRPH1004 <91398006+DRPH1004@users.noreply.github.com>
2021-11-22 18:29:28 -0600

    Create sol1.js

---
+++ 2p/2queue/2do/sol1.js
@@ -0,0 +1,58 @@
+
+   
+function Queue()
+{
+    this.DataStore=[]
+    this.enqueue=enqueue
+    this.dequeue=dequeue
+    this.front=front
+    this.back=back
+    this.toString=toString
+    this.empty=empty
+}
+function enqueue(element)
+{
+    return this.DataStore.push(element)
+}
+function dequeue()
+{
+    return this.DataStore.shift()
+}
+function front()
+{
+    return this.DataStore[0]
+}
+function back()
+{
+    return this.DataStore[this.DataStore.length - 1]
+}
+function toString()
+{
+    var retStr=""
+    for (var i=0;i<this.DataStore.length;i++)
+    {
+        retStr+=this.DataStore[i]+"\n"
+    }
+    return retStr
+}
+function empty()
+{
+    if(this.DataStore.length==0)
+    {
+        return true
+    }
+    else
+    {
+        return false
+    }
+}
+//Program Testing
+var qtest=new Queue()
+qtest.enqueue("Creel, Chihuahua")
+qtest.enqueue("Bernal, Querétaro")
+qtest.enqueue("Pátzcuaro, Michoacán")
+console.log(qtest.toString())
+qtest.dequeue()
+console.log(qtest.toString())
+console.log("Front of Queue: "+qtest.front())
+console.log("Back of Queue: "+qtest.back())
